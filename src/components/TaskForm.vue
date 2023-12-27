e7634324f3c2509abf815b9ec06bf83cd6f54a5a
MdShijanAli <freelancershijan@gmail.com>
2023-12-27 23:51:39 +0600

    TaskForm component done

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <div >
+
+
+<input type="checkbox" :id="modal" class="modal-toggle" />
+<div class="modal" role="dialog">
+  <div class="modal-box rounded-sm p-10">
+
+  <h2 class="text-center text-3xl font-semibold mb-5">Todo From</h2>
+
+    <form @submit.prevent="handleSubmit">
+
+      <div>
+        <label for="title">Title</label>
+      <input class="px-5 py-2 block my-2 w-full border " type="text" placeholder="Enter Todo Title" v-model="todoData.title">
+      </div>
+      <div class="mt-5">
+        <label for="title">Description</label>
+      <textarea class="px-5 py-2 block my-2 w-full border " type="text" placeholder="Enter Todo Title" v-model="todoData.description" rows="5"></textarea>
+       </div>
+
+       
+        <div class="modal-action flex items-center justify-end gap-3 mt-5">
+          <label :for="modal" class="btn px-5 py-3 text-white bg-black hover:bg-black rounded-sm">Cancel</label>
+          <button type="submit" class="px-5 py-3 text-white bg-green-600 rounded-sm">Add</button>
+        </div>
+
+    </form>
+  </div>
+  <label class="modal-backdrop" :for="modal">Close</label>
+</div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    modal: {
+      type: String,
+      default: null
+    }
+  },
+  name: "TaskForm",
+
+  data() {
+    return {
+      todoData: {
+        title: ""
+      }
+    }
+  },
+
+  methods: {
+    handleSubmit() {
+      this.$emit('todoData', this.todoData)
+    }
+  }
+}
+</script>
+<style>
+  
+</style>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -3,7 +3,7 @@
    <div class="max-w-7xl mx-auto px-6 py-20">
        <div class="flex items-center justify-between mb-10">
         <h2 class="text-3xl font-semibold">Todo App</h2>
-        <button class="px-5 py-3 bg-green-600 font-semibold text-white hover:bg-green-700 transition duraiton-500 ease-in-out">Add New</button>
+        <label for="toDoFormModal" class="px-5 py-3 bg-green-600 font-semibold text-white hover:bg-green-700 transition duraiton-500 ease-in-out">Add New</label>
        </div>
         <div class="grid grid-cols-3 gap-10">
            <div v-for="todo in todos" :key="todo.id">
@@ -11,6 +11,8 @@
            </div>
         </div>
 
+
+        <TaskForm modal="toDoFormModal" @todoData="handleTodoData"/>
    </div>
 </div>
 </template>
@@ -18,13 +20,22 @@
 import Task from './Task.vue';
 import { mapState } from 'pinia';
 import { useTodoStore } from '../stores/todo';
+import TaskForm from './TaskForm.vue'
 export default {
-  components: { Task },
+  components: { Task,TaskForm },
   name: "TaskList",
 
   data() {
     return {
-      
+     
+    }
+  },
+
+  methods: {
+    handleTodoData(todoData) {
+      // Handle the emitted todoData here
+      console.log('Received todoData:', todoData);
+      this.todos.push(todoData)
     }
   },
 
@@ -36,6 +47,7 @@ export default {
 
 
   },
+  
 
 
 }
